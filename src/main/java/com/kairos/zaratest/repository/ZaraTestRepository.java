b78fb0b3ba43753cd551a23820e11cc235761a24
Alejandro <alejandro@desktop>
2020-06-26 14:22:25 +0200

    It's reading from the repository correctlyç

--- src/main/java/com/kairos/zaratest/repository/ZaraTestRepository.java
+++ src/main/java/com/kairos/zaratest/repository/ZaraTestRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ZaraTestRepository extends CrudRepository<PriceInfo, Integer> {
-
+    Iterable<PriceInfo> findAll();
 }
--- src/main/java/com/kairos/zaratest/service/ZaraTestService.java
+++ src/main/java/com/kairos/zaratest/service/ZaraTestService.java
@@ -1,20 +1,27 @@
 package com.kairos.zaratest.service;
 
 import com.kairos.zaratest.domain.PriceInformationResponse;
+import com.kairos.zaratest.model.PriceInfo;
 import com.kairos.zaratest.repository.ZaraTestRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ZaraTestService {
-    @Autowired
     private ZaraTestRepository zaraTestRepository;
 
-    public ZaraTestService() {
-
+    @Autowired
+    public ZaraTestService(ZaraTestRepository zaraTestRepository) {
+        this.zaraTestRepository = zaraTestRepository;
     }
 
     public PriceInformationResponse getPriceInformation() {
+        List<PriceInfo> all = (List<PriceInfo>) zaraTestRepository.findAll();
+
+        System.out.println(all);
+
         return null;
     }
 }
